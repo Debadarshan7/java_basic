94db1b3024f70103613e56e689bbc8b9a7b8be58
Debadarshan7 <165816550+Debadarshan7@users.noreply.github.com>
2024-04-03 22:35:16 +0530

    Add files via upload

---
+++ data.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class data{
+    public static void main(String[] args)  throws Exception{
+    FileReader f= new FileReader("raja.txt");
+    int i ;
+    while ((i = f.read()) != -1) {
+
+    }
+    }
+}
